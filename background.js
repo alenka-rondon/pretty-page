e0541d28fb7da79cb2e7a8c1a6db45bc06c7ab05
alenka-rondon <85842242+alenka-rondon@users.noreply.github.com>
2022-04-27 19:13:36 -0400

    adding presets, and color input

--- background.js
+++ background.js
@@ -11,6 +11,8 @@ window.addEventListener("load", function()
 {
   document.getElementById("font-select")
           .addEventListener("click", update, false);
+    document.getElementById("preset-select")
+          .addEventListener("click", updatePreset, false);
 }, false);
 
 
@@ -55,6 +57,131 @@ function update() {
             }
         }
 
+
+        
+
 }
 update();
+
+
+function updatePreset() {
+
+    var select = document.getElementById('preset-select');
+    var option = select.options[select.selectedIndex];
+
+    document.getElementById('presetValue').value = option.value;
+
+    // document.getElementById('text').value = option.text;
+
+    var selectedPreset = option.value; //grabbing new font 
+
+    //Snag our button
+    let choosePreset = document.getElementById("presetSelect") ;
+
+    // Set our button's color to the color that we stored
+    chrome.storage.sync.get("color", ({color}) => {
+        choosePreset.style.backgroundColor = color
+    })
+
+    if (selectedPreset === "dark-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#1f1f1f";
+                        elems[i].style.color = "#def3fc";
+                    }
+                }
+            })
+        })
+
+    } else if (selectedPreset === "princess-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#ebeae6";
+                        elems[i].style.color = "#78235d";
+                    }
+                }
+            })
+        })
+
+    } else if (selectedPreset === "vader-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#303030";
+                        elems[i].style.color = "#bf1b2b";
+                    }
+                }
+            })
+        })
+
+    } else if (selectedPreset === "pastel1-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#949494";
+                        elems[i].style.color = "#BFE8DE";
+                    }
+                }
+            })
+        })
+
+    } else if (selectedPreset === "pastel2-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#E1EfE1";
+                        elems[i].style.color = "#b497b4";
+                    }
+                }
+            })
+        })
+
+    } else if (selectedPreset === "blue-mode") {
+        choosePreset.addEventListener("click", async () => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+            
+            chrome.scripting.executeScript({ // Run the following script on our tab
+                target: {tabId: tab.id},
+                function: () => {
+                    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+                    for (var i = 0;i < elems.length; i++){ 
+                        elems[i].style.backgroundColor = "#070831";
+                        elems[i].style.color = "#aadad9";
+                    }
+                }
+            })
+        })
+
+    }
+
+
+}
+updatePreset();
 
--- buttons.js
+++ buttons.js
@@ -1,49 +1,3 @@
-// get button from html file 
-let pinkFontBtn = document.getElementById("changeColor")
-
-chrome.storage.sync.get("color", ({color}) => {
-    pinkFontBtn.style.backgroundColor = color
-})
-
-// Run on click
-pinkFontBtn.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
-
-    chrome.scripting.executeScript({ // Run the following script on our tab
-        target: {tabId: tab.id},
-        function: () => {
-            let elems = document.querySelectorAll("*"); // changing every element
-            for (var i = 0;i < elems.length; i++){ 
-                elems[i].style.color = "#932962";
-            }
-        }
-    })
-})
-
-// Snag our button
-let fontBtn = document.getElementById("changeFont")
-
-// Set our button's color to the color that we stored
-chrome.storage.sync.get("color", ({color}) => {
-    fontBtn.style.backgroundColor = color
-})
-
-// Run on click
-fontBtn.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
-
-    chrome.scripting.executeScript({ // Run the following script on our tab
-        target: {tabId: tab.id},
-        function: () => {
-            let elems = document.querySelectorAll("*"); // Grab every element in the dom
-            for (var i = 0;i < elems.length; i++){ 
-                elems[i].style.fontFamily = "cursive";
-            }
-        }
-    })
-})
-
-
 // Snag our button
 let sizeBtn = document.getElementById("changeSize")
 
@@ -66,3 +20,55 @@ sizeBtn.addEventListener("click", async () => {
         }
     })
 })
+
+
+let bgBtn = document.getElementById("bgColor");
+
+chrome.storage.sync.get("color", ({color}) => {
+    bgBtn.style.backgroundColor = color
+})
+
+bgBtn.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}); // Find current tab
+    const color = document.getElementById("colorChoice").value;
+
+    chrome.scripting.executeScript({ // Run the following script on our tab
+        target: {tabId: tab.id},
+        function: changeBackgroundColor,
+        args: [color]
+        
+    })
+})
+
+function changeBackgroundColor (color) {
+    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+        for (var i = 0;i < elems.length; i++){ 
+            elems[i].style.backgroundColor = color;
+        }
+}
+
+
+let fontColorBtn = document.getElementById("fontColor");
+
+chrome.storage.sync.get("color", ({color}) => {
+    fontColorBtn.style.backgroundColor = color
+})
+
+fontColorBtn.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}); // Find current tab
+    const color = document.getElementById("fontColorChoice").value;
+
+    chrome.scripting.executeScript({ // Run the following script on our tab
+        target: {tabId: tab.id},
+        function: changeFontColor,
+        args: [color]
+        
+    })
+})
+
+function changeFontColor (color) {
+    let elems = document.querySelectorAll("*"); // Grab every element in the dom
+        for (var i = 0;i < elems.length; i++){ 
+            elems[i].style.color = color;
+        }
+}
